f6002eb50cbbf533240b8ce9c87114b307d6e9fc
Chakibceran22 <grabachakib008@gmail.com>
2025-02-02 18:47:44 +0100

    basic function will add more later

--- http_test.py
+++ http_test.py
@@ -3,15 +3,25 @@ import requests
 
 def check_website(url):
     try:
-        response = requests.head(url)
-        if response.status_code == 200:
-            logging.info(f"URL {url} is up and running")
+        responseHead = requests.head(url, timeout= 2)# to not keep the request just hanging there
+        if responseHead.status_code == 200:
+            logging.info("Server is ALive")
         else:
-            logging.warning(f"URL {url} returned a status code of {response.status_code} ")
-    except:
-        logging.error(f"error happened")
+            logging.warning(f"URL {url} returned a status code of {responseHead.status_code} ")
+        responseGet = requests.get(url, timeout= 2)
+        logging.info(f"The Server Technologies are: {responseGet.headers.get('Server')}")
+
+        responseOptions = requests.options(url, timeout= 2)
+        logging.info(f"Allowed Methods: {responseOptions.headers.get('Allow')}")
+    except requests.exceptions.ConnectionError:
+        logging.error("Connection error accured")
+    except requests.exceptions.Timeout:
+        logging.error("Timeout error accured")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"An error accured: {e}")
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     check_website("https://www.google.com")
-    check_website("https://www.google.com/404")
+    check_website("https://httpbin.org/delay/5")
